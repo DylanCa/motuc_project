9e7ec2d08125a19d996aec2425f7a634ce336956
DylanCa <dylan.cattelan@gmail.com>
2019-04-05 17:11:55 +0200

    Added functions to open files & deal with them

---
+++ Cryptography/python-uncipherer/filefunctions.py
@@ -0,0 +1,16 @@
+class FileFunction():
+    filepath = ''
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def openfile(self, encoding="utf-8"):
+        f = open(self.filepath, encoding=encoding)
+        return f.readlines()
+
+    def return6charwordsfromfile(self, f):
+        return [
+            word.strip('\n') for word in f if len(word.replace('\n', '')) == 6
+        ]
+
+
