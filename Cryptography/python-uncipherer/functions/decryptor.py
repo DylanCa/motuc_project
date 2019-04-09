22609ef75375b895c24c528bb3256473e145f544
DylanCa <dylan.cattelan@gmail.com>
2019-04-09 10:48:01 +0200

    Added the correct key

--- Cryptography/python-uncipherer/functions/decryptor.py
+++ Cryptography/python-uncipherer/functions/decryptor.py
@@ -5,12 +5,12 @@ from itertools import cycle
 # f = input("Enter the name of the file: ")
 # key = input("Enter the 6-char key, please: ")
 
-f = 'PB0.txt'
-key = "babltq"
+f = 'PK.txt'
+key = "chfyjq"
 
 EncryptedFileFunctions = FileFunction(f)
 encrypted_file = EncryptedFileFunctions.openfile()
-WriteInFileFunctions = FileFunction("crypted_" + f)
+WriteInFileFunctions = FileFunction("decrypted_" + f)
 
 text = []
 for char, keyletter in zip(encrypted_file, cycle(key)):
